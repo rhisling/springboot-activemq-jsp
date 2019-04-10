20ea0fa521b45aa16921460ebf46c3e3db0132db
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-04-10 00:05:33 -0700

    Adding transaction management with dead letter queue

--- src/main/java/me/aravindh/jms/activemqmessageconverters/config/JmsConfig.java
+++ src/main/java/me/aravindh/jms/activemqmessageconverters/config/JmsConfig.java
@@ -1,6 +1,8 @@
 package me.aravindh.jms.activemqmessageconverters.config;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,7 @@ import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.connection.JmsTransactionManager;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
@@ -18,7 +21,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJms
 @EnableTransactionManagement
 @Configuration
-public class JmsConfig { //implements JmsListenerConfigurer
+public class JmsConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JmsConfig.class);
 
     @Value("${spring.activemq.broker-url}")
     private String brokerUrl;
@@ -28,7 +33,6 @@ public class JmsConfig { //implements JmsListenerConfigurer
 
     @Value("${spring.activemq.password}")
     private String password;
-
 
     @Bean
     public MessageConverter jacksonJmsMessageConverter() {
@@ -41,35 +45,39 @@ public class JmsConfig { //implements JmsListenerConfigurer
 
     @Bean
     public CachingConnectionFactory connectionFactory() {
-
         CachingConnectionFactory factory = new CachingConnectionFactory(
                 new ActiveMQConnectionFactory(user, password, brokerUrl)
         );
-        // factory.setReconnectOnException(true);
         factory.setClientId("StoreFront");
         factory.setSessionCacheSize(100);
         return factory;
     }
+
 
     @Bean
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory());
         factory.setMessageConverter(jacksonJmsMessageConverter());
-        //factory.setMessageConverter(xmlMarshallingMessageConverter());
         factory.setTransactionManager(jmsTransactionManager());
+        factory.setErrorHandler(t -> {
+            LOGGER.info("Handling error in listening for messages, error: " + t.getMessage());
+        });
         return factory;
     }
 
-    /**
-     * On Rollback msg is not sent by producer
-     * on consumer side, msg is re-queued on the MOM for retrieval.
-     * Re-queueing of messages is broker dependent
-     * For ActiveMQ,on rollback, after retries you can configure the broker to move it to DLQ(Dead letter queue)
-     */
     @Bean
     public PlatformTransactionManager jmsTransactionManager() {
         return new JmsTransactionManager(connectionFactory());
+    }
+
+    @Bean
+    public JmsTemplate jmsTemplate() {
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory());
+        jmsTemplate.setMessageConverter(jacksonJmsMessageConverter());
+        jmsTemplate.setDeliveryPersistent(true);
+        jmsTemplate.setSessionTransacted(true);
+        return jmsTemplate;
     }
 
 
--- src/main/java/me/aravindh/jms/activemqmessageconverters/service/jms/WarehouseReceiverService.java
+++ src/main/java/me/aravindh/jms/activemqmessageconverters/service/jms/WarehouseReceiverService.java
@@ -3,6 +3,7 @@ package me.aravindh.jms.activemqmessageconverters.service.jms;
 import me.aravindh.jms.activemqmessageconverters.model.BookOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,17 @@ public class WarehouseReceiverService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WarehouseReceiverService.class);
 
+    @Autowired
+    private WarehouseProcessingService warehouseProcessingService;
+
     @JmsListener(destination = "book.order.queue")
     public void receive(BookOrder bookOrder) {
         LOGGER.info("received a message");
         LOGGER.info("Message is == " + bookOrder);
-
+        if(bookOrder.getBook().getTitle().startsWith("L")){
+            throw new RuntimeException("bookOrderId=" + bookOrder.getBookOrderId() + " is of a book not allowed!");
+        }
+        warehouseProcessingService.processOrder(bookOrder);
 
     }
 }
