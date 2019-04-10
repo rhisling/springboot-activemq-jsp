6d05157e8c030466b130d015ef1aef8a1537179f
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-04-10 00:32:42 -0700

    response mgmt

--- src/main/java/me/aravindh/jms/activemqmessageconverters/service/jms/WarehouseReceiverService.java
+++ src/main/java/me/aravindh/jms/activemqmessageconverters/service/jms/WarehouseReceiverService.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,14 +18,27 @@ public class WarehouseReceiverService {
     @Autowired
     private WarehouseProcessingService warehouseProcessingService;
 
+    /**
+     * Using JMS headers without modifying pojos
+     * @param bookOrder
+     * @param orderState
+     * @param bookOrderId
+     * @param storeId
+     * @param messageHeaders
+     */
     @JmsListener(destination = "book.order.queue")
-    public void receive(BookOrder bookOrder) {
+    public void receive(@Payload BookOrder bookOrder, @Header(name = "orderState") String orderState,
+                        @Header(name = "bookOrderId") String bookOrderId, @Header(name = "storeId") String storeId,
+                        MessageHeaders messageHeaders) {
         LOGGER.info("received a message");
         LOGGER.info("Message is == " + bookOrder);
-        if(bookOrder.getBook().getTitle().startsWith("L")){
+        LOGGER.info("Message property  orderState = {} , bookOrderId= {}, storeId = {}", orderState, bookOrderId,
+                storeId);
+        LOGGER.info("messageHeaders = {}", messageHeaders);
+        if (bookOrder.getBook().getTitle().startsWith("L")) {
             throw new RuntimeException("bookOrderId=" + bookOrder.getBookOrderId() + " is of a book not allowed!");
         }
-        warehouseProcessingService.processOrder(bookOrder);
+        warehouseProcessingService.processOrder(bookOrder, orderState, storeId);
 
     }
 }
