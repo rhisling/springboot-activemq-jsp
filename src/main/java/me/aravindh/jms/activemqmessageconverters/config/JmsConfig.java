05235ec31e89e0a467ceca9cd3dcfb8cb35ac179
Aravindhan Elayakumar <aravindhansai@gmail.com>
2019-04-09 23:36:44 -0700

    Adding single connection factory

--- src/main/java/me/aravindh/jms/activemqmessageconverters/config/JmsConfig.java
+++ src/main/java/me/aravindh/jms/activemqmessageconverters/config/JmsConfig.java
@@ -1,35 +1,32 @@
 package me.aravindh.jms.activemqmessageconverters.config;
 
-import me.aravindh.jms.activemqmessageconverters.listeners.BookOrderProcessingMessageListener;
-import me.aravindh.jms.activemqmessageconverters.model.Book;
-import me.aravindh.jms.activemqmessageconverters.model.BookOrder;
-import me.aravindh.jms.activemqmessageconverters.model.Customer;
 import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.annotation.JmsListenerConfigurer;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerEndpointRegistrar;
-import org.springframework.jms.config.SimpleJmsListenerEndpoint;
+import org.springframework.jms.connection.SingleConnectionFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MarshallingMessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
-import org.springframework.oxm.xstream.XStreamMarshaller;
-
-import javax.jms.ConnectionFactory;
 
 
 @EnableJms
 @Configuration
-public class JmsConfig{ //implements JmsListenerConfigurer
+public class JmsConfig { //implements JmsListenerConfigurer
 
-    @Autowired
-    public ConnectionFactory connectionFactory;
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
 
-    // @Bean
+    @Value("${spring.activemq.user}")
+    private String user;
+
+    @Value("${spring.activemq.password}")
+    private String password;
+
+
+    @Bean
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
@@ -37,59 +34,26 @@ public class JmsConfig{ //implements JmsListenerConfigurer
         return converter;
     }
 
-    @Bean
-    public MessageConverter xmlMarshallingMessageConverter() {
-        MarshallingMessageConverter converter = new MarshallingMessageConverter(xmlMarshaller());
-        converter.setTargetType(MessageType.TEXT);
-        return converter;
-    }
 
     @Bean
-    public XStreamMarshaller xmlMarshaller() {
-        XStreamMarshaller marshaller = new XStreamMarshaller();
-        marshaller.setSupportedClasses(Book.class, Customer.class, BookOrder.class);
-        return marshaller;
-    }
+    public SingleConnectionFactory connectionFactory() {
 
-  /*  @Bean
-    public ActiveMQConnectionFactory connectionFactory() {
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+        SingleConnectionFactory factory = new SingleConnectionFactory(
+                new ActiveMQConnectionFactory(user, password, brokerUrl)
+        );
+        factory.setReconnectOnException(true);
+        factory.setClientId("StoreFront");
         return factory;
-    }*/
-
-    /*@Bean
-    public JmsTemplate jmsTemplate() {
-        JmsTemplate template = new JmsTemplate();
-        template.setConnectionFactory(connectionFactory());
-        template.setMessageConverter(jacksonJmsMessageConverter());
-        return template;
-    }*/
+    }
 
     @Bean
     public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        //factory.setMessageConverter(jacksonJmsMessageConverter());
-        factory.setMessageConverter(xmlMarshallingMessageConverter());
+        factory.setConnectionFactory(connectionFactory());
+        factory.setMessageConverter(jacksonJmsMessageConverter());
+        //factory.setMessageConverter(xmlMarshallingMessageConverter());
         return factory;
     }
 
-    @Bean
-    public BookOrderProcessingMessageListener jmsMessageListener() {
-        BookOrderProcessingMessageListener listener = new BookOrderProcessingMessageListener();
-        return listener;
-    }
 
-
-   /* @Override
-    public void configureJmsListeners(JmsListenerEndpointRegistrar registrar) {
-        SimpleJmsListenerEndpoint endpoint = new SimpleJmsListenerEndpoint();
-        endpoint.setMessageListener(jmsMessageListener());
-        endpoint.setDestination("book.order.processed.queue");
-        endpoint.setId("book-order-processed-queue");
-        endpoint.setSubscription("my-subscription");
-        endpoint.setConcurrency("1");
-        registrar.setContainerFactory(jmsListenerContainerFactory());
-        registrar.registerEndpoint(endpoint,jmsListenerContainerFactory());
-    }*/
 }
